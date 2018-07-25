c5febb33e78410f3b1e47482401e6fee4f60390d
Jossué Dután <jossuedutan@gmail.com>
2018-07-25 09:04:28 -0500

    Arreglada la pantalla de inicio

---
+++ DiscoveringMyCity/DiscoveringMyCity/TabBarViewControllerJ.swift
@@ -0,0 +1,50 @@
+//
+//  TabBarViewControllerJ.swift
+//  DiscoveringMyCity
+//
+//  Created by Jossué Dután on 25/7/18.
+//  Copyright © 2018 Optativa Profesional. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewControllerJ: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) { //Did appear sobreescribe lo cargado en la vista
+        configTabBar()
+    }
+    
+    func configTabBar(){
+        let items = tabBar.items
+        items?[1].title = "Perfil"
+        items?[2].title = "Añadir"
+        items?[3].title = "Configuración"
+        items?[1].image = #imageLiteral(resourceName: "user_male")
+        items?[2].image = #imageLiteral(resourceName: "plus")
+        items?[3].image = #imageLiteral(resourceName: "services")
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
