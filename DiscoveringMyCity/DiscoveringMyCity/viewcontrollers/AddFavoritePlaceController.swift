b3807f3b9997255a651b7d70f89dac807b1edb5b
Jossue <jossuedutan@gmail.com>
2018-08-07 12:57:19 -0500

    Arreglada vista Agregar (funciona importar imagen)

--- DiscoveringMyCity/DiscoveringMyCity/viewcontrollers/AddFavoritePlaceController.swift
+++ DiscoveringMyCity/DiscoveringMyCity/viewcontrollers/AddFavoritePlaceController.swift
@@ -6,4 +6,49 @@
 //  Copyright © 2018 Optativa Profesional. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class AddFavoritePlaceController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var categoryField: UITextField!
+    @IBOutlet weak var latitudField: UITextField!
+    @IBOutlet weak var longitudField: UITextField!
+    @IBOutlet weak var imagenView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func photoButton(_ sender: Any) {
+        let image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        image.allowsEditing = false
+        
+        self.present(image, animated: true){
+            //Después complete
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        {
+            imagenView.image = image
+        }
+        else
+        {
+            print("Error imagen")
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func locationButton(_ sender: Any) {
+    }
+    
+    @IBAction func saveButton(_ sender: Any) {
+    }
+    
+}
