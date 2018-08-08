be797ce3f3132be66d10fc4972d10c3929c96bb5
Jossue <jossuedutan@gmail.com>
2018-08-08 10:24:38 -0500

    Funcionando favoritos, añadir y corregidos los constraints

--- DiscoveringMyCity/DiscoveringMyCity/FavoritesTableViewCell.swift
+++ DiscoveringMyCity/DiscoveringMyCity/FavoritesTableViewCell.swift
@@ -13,7 +13,6 @@ class FavoritesTableViewCell: UITableViewCell {
     
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var categoryLabel: UILabel!
-    @IBOutlet weak var favimageView: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
